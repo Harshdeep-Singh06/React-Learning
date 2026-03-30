6311d54073df02e8c8ba4d7861443710c89dc675
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-03-30 14:28:01 +0530

    Conditional rendring by if else conditions

--- react-project-six/src/App.jsx
+++ react-project-six/src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import Logout from './components/Logout';
+import Login from './components/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
+ const[isLoggedIn, setLoggedIn] = useState(false);
 
-  return (
-   <div>
-      
-   </div>
-  )
+  if(isLoggedIn){
+    return (
+      <Logout/>
+    )
+  }
+  else{
+    return(
+      <Login/>
+    )
+  }
+
 }
 
 export default App
--- react-project-six/src/components/Login.jsx
+++ react-project-six/src/components/Login.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Login = () => {
   return (
-   <buttton>
+   <button className=''>
     Login
-   </buttton>
+   </button>
   )
 }
 
