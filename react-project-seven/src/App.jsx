548af2876b768d4966a050231c2aab97bad1adbd
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-03-30 23:47:01 +0530

    Add event listener on para

--- react-project-seven/src/App.jsx
+++ react-project-seven/src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+
   function handleClick(){
     alert("I am Clicked");
   }
 
+
+  function handleMouseOver(){
+    alert("Hey buddy");
+  }
+
+
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
-      <p className='font-bold font-serif text-pink-500'>
+      <p className='font-bold font-serif text-pink-500' onMouseOver={handleMouseOver}>
         Hey buddy para here!
       </p>
       <button className='rounded-lg bg-blue-400' onClick={handleClick}>
