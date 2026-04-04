67d80a3470f6b91d3ccc58de497f83a837ea06ad
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-04 22:53:58 +0530

    Add validation for input field

--- react-form-project/src/App.jsx
+++ react-form-project/src/App.jsx
@@ -22,22 +22,37 @@ function App() {
 
     <div className='mb-4'>
       <label className='text-white text-xl font-serif shadow-xl shadow-sky-700'>First Name: </label>
-      <input {...register("firstName")} className='shadow-xl shadow-sky-900 border-2 border-sky-500 outline-none rounded-2xl ml-2 px-3 text-white'/>
+      <input {...register("FirstName", { required:"First Name is required",
+      minLength:{value:3, message:'Minimum length atleast 3'},
+       maxLength:{value:6, message: "Maximum length atleast 6"}
+      })} className='shadow-xl shadow-sky-900 border-2 border-sky-500 outline-none rounded-2xl ml-2 px-3 text-white  focus:border-sky-300'/>
+      {errors.FirstName && <p className='font-semibold text-sky-200 ml-20 mt-3' >{errors.FirstName.message}</p>}
     </div>
 
     <div className='mb-4'>
       <label className='text-white text-xl font-serif shadow-xl shadow-sky-700'>Middle Name: </label>
-      <input {...register("MiddleName")} className='shadow-xl shadow-sky-900 border-2 border-sky-500 outline-none rounded-2xl ml-2 px-3 text-white'/>
+      <input {...register("MiddleName", { required:"Middle Name is required",
+      minLength:{value:3, message:'Minimum length atleast 3'},
+       maxLength:{value:6, message: "Maximum length atleast 6"}
+      })} className='shadow-xl shadow-sky-900 border-2 border-sky-500 outline-none rounded-2xl ml-2 px-3 text-white  focus:border-sky-300'/>
+      {errors.MiddleName && <p className='font-semibold text-sky-200 ml-20 mt-3' >{errors.MiddleName.message}</p>}
     </div>
 
     <div className='mb-4'>
       <label className='text-white text-xl font-serif shadow-xl shadow-sky-700'>Last Name: </label>
-      <input {...register("LastName")} className='shadow-xl shadow-sky-900 border-2 border-sky-500 outline-none rounded-2xl ml-2 px-3 text-white'/>
+      <input {...register("LastName",
+      { required:"Last Name is required",
+      minLength:{value:3, message:'Minimum length atleast 3'},
+       maxLength:{value:6, message: "Maximum length atleast 6"}
+      })} 
+      className='shadow-xl shadow-sky-900 border-2 border-sky-500 outline-none rounded-2xl ml-2 px-3 text-white focus:border-sky-300'/>
+      {errors.LastName && <p className='font-semibold text-sky-200 ml-20 mt-3' >{errors.LastName.message}</p>}
     </div>
 
      <button 
           type="submit"
-          className="mt-4 ml-33 border-4 border-sky-500 text-white font-semibold px-4 py-1 rounded-2xl shadow-sky-900 text-sm hover:font-semibold hover:border-4 hover:border-sky-300 transition">
+          className="mt-4 ml-33 border-4 border-sky-500 text-sky-400 font-semibold px-4 py-1 rounded-2xl shadow-sky-900 text-sm hover:font-semibold hover:border-sky-300 hover:text-sky-200
+          active:translate-y-2 transition">
           Submit
         </button>
 
