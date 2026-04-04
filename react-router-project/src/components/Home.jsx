04aef4b5f4819dc510faf0f41ba04d91b660cc7e
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-04 14:40:16 +0530

    feat: add button navigation from Home to About

--- react-router-project/src/components/Home.jsx
+++ react-router-project/src/components/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+
+  const navigate = useNavigate();
+  function handleClick(){
+      navigate('/about');
+  }
   return (
     <div>
         Home Page
+        <button onClick={handleClick} className='border border-amber-300 rounded-2xl px-2 font-sm shadow-xl mt-4 py-1 ml-4'>
+          Move to About Page
+        </button>
     </div>
   )
 }
