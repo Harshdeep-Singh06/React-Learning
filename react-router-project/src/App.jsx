cdf6457bfd5beb877893c17be65133e988e7f6b6
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-04 08:03:20 +0530

    Create a route

--- react-router-project/src/App.jsx
+++ react-router-project/src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
-
 import './App.css'
+import {createBrowserRouter} from "react-router-dom"
+import Home from './components/Home'
+
+const router = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element:<Home/>
+    },
+    {},
+    {}
+  ]
+)
 
 function App() {
-  
+
 
   return (
-    <>
+    <div>
       Hello Jee
-    </>
+    </div>
   )
 }
 
