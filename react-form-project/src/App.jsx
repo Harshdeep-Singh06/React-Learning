941884ec175e720ab55a4d8807fd1ac84ebc2375
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-04 21:32:19 +0530

    create button

--- react-form-project/src/App.jsx
+++ react-form-project/src/App.jsx
@@ -11,28 +11,37 @@ function App() {
     formState: { errors },
   } = useForm();
 
+  function onSubmit(data){
+    console.log("Submitting the form",data )
+  }
+
   return (
    <div className='flex flex-col justify-center items-center h-screen border bg-black'>
 
-     <form className='px-10 py-10 rounded-2xl shadow-2xl shadow-sky-900'>
+     <form onSubmit={handleSubmit(onSubmit)} className='px-10 py-10 rounded-2xl shadow-2xl shadow-sky-900 border-3 border-sky-700'>
 
     <div className='mb-4'>
       <label className='text-white text-xl font-serif shadow-xl shadow-sky-700'>First Name: </label>
-      <input className='shadow-xl shadow-sky-900 border-2 border-sky-500 outline-none rounded-2xl ml-2 px-3 text-white'/>
+      <input {...register("firstName")} className='shadow-xl shadow-sky-900 border-2 border-sky-500 outline-none rounded-2xl ml-2 px-3 text-white'/>
     </div>
 
     <div className='mb-4'>
       <label className='text-white text-xl font-serif shadow-xl shadow-sky-700'>Middle Name: </label>
-      <input className='shadow-xl shadow-sky-900 border-2 border-sky-500 outline-none rounded-2xl ml-2 px-3 text-white'/>
+      <input {...register("MiddleName")} className='shadow-xl shadow-sky-900 border-2 border-sky-500 outline-none rounded-2xl ml-2 px-3 text-white'/>
     </div>
 
     <div className='mb-4'>
       <label className='text-white text-xl font-serif shadow-xl shadow-sky-700'>Last Name: </label>
-      <input className='shadow-xl shadow-sky-900 border-2 border-sky-500 outline-none rounded-2xl ml-2 px-3 text-white'/>
+      <input {...register("LastName")} className='shadow-xl shadow-sky-900 border-2 border-sky-500 outline-none rounded-2xl ml-2 px-3 text-white'/>
     </div>
 
+     <button 
+          type="submit"
+          className="mt-4 ml-33 border-4 border-sky-500 text-white font-semibold px-4 py-1 rounded-2xl shadow-sky-900 text-sm hover:font-semibold hover:border-4 hover:border-sky-300 transition">
+          Submit
+        </button>
 
-   </form>
+  </form>
    </div>
   )
 }
