96b4871e63bd359dd40496beb983d67889c51db3
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-04 08:34:09 +0530

    feat: add routing and navigation using react-router-dom

--- react-router-project/src/App.jsx
+++ react-router-project/src/App.jsx
@@ -1,16 +1,37 @@
 import { useState } from 'react'
 import './App.css'
-import {createBrowserRouter} from "react-router-dom"
+import {createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from './components/Home'
+import About from './components/About'
+import Dashboard from './components/Dashboard'
+import Navbar from './components/Navbar'
 
 const router = createBrowserRouter(
   [
     {
       path:"/",
-      element:<Home/>
+      element:
+      <div>
+        <Navbar/>
+        <Home/>
+      </div>
     },
-    {},
-    {}
+    {
+      path:"/about",
+      element:
+      <div>
+        <Navbar/>
+        <About/>
+      </div> 
+    },
+    {
+      path:"/dashboard",
+      element:
+      <div>
+        <Navbar/>
+        <Dashboard/>
+      </div>
+    }
   ]
 )
 
@@ -19,7 +40,7 @@ function App() {
 
   return (
     <div>
-      Hello Jee
+     <RouterProvider router = {router}/>
     </div>
   )
 }
