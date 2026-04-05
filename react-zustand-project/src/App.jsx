fa1d78b136d1ac871ec8f89ff19b649e1dc647da
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-05 17:17:27 +0530

    Create new project folder to learn Zustand

---
+++ react-zustand-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+  return (
+   <div className='bg-gray-900 min-h-screen'> 
+   <h1 className='text-white text-2xl'>
+    Hello jee
+   </h1>
+   </div>
+  )
+}
+
+export default App
