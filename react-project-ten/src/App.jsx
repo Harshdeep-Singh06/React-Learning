1d9220f048193c215f46f0e18adbd6255bf54e09
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-06 20:09:08 +0530

    Add increment button

--- react-project-ten/src/App.jsx
+++ react-project-ten/src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [count, setCount] = useState(0);
   return (
    <div className='flex flex-col items-center justify-center bg-black min-h-screen'>
-    <h1 className='bg-sky-500 px-4 py-2 rounded-xl '>
-      Hello jee
-    </h1>
+    <button className='bg-sky-500 px-4 py-2 rounded-xl border-4 border-gray-800 active:translate-y-2 transition'>
+      Increment
+    </button>
    </div>
   )
 }
