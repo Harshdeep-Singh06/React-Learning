f5296cecb5fe097292445b00e665535bd51882d8
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-06 20:02:45 +0530

    Add new project folder ten

---
+++ react-project-ten/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+   <div className='flex flex-col items-center justify-center bg-black min-h-screen'>
+    <h1 className='bg-sky-500 px-4 py-2 rounded-xl '>
+      Hello jee
+    </h1>
+   </div>
+  )
+}
+
+export default App
