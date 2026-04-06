08bca4cd422980a371c35f37b243d0ddefb36fbf
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-06 20:47:38 +0530

    add counter with useState and useRef

--- react-project-ten/src/App.jsx
+++ react-project-ten/src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
+import { useState,useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
+
+  let val = useRef(0);
+
+  function handleIncrement(){
+    val.current = val.current + 1;
+    console.log("Value of val:",val.current);
+    setCount(count+1);
+  }
+
+  //It runs on every render
+  useEffect(()=>{
+    console.log("Render Again");
+  })
+
   return (
    <div className='flex flex-col items-center justify-center bg-black min-h-screen'>
-    <button className='bg-sky-500 px-4 py-2 rounded-xl border-4 border-gray-800 active:translate-y-2 transition'>
+    <button
+    onClick={handleIncrement}
+     className='bg-sky-500 px-4 py-2 rounded-xl border-4 border-gray-800 active:translate-y-2 transition'>
       Increment
     </button>
+    <div className='bg-sky-500 rounded-xl px-4 py-2 mt-4'>
+      Count: {count}
+    </div>
    </div>
   )
 }
