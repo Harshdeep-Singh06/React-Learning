8be8e4e910dba01449f388e6f8a3aec3ab6827d6
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-06 14:07:19 +0530

    choose subscription of change state

--- react-zustand-project/src/components/Navbar.jsx
+++ react-zustand-project/src/components/Navbar.jsx
@@ -1,31 +1,29 @@
 import React from 'react'
 import useMystore from '../store'
+import { useEffect } from 'react'
 
 const Navbar = () => {
 
-    const state = useMystore();
-    // console.log(state)
+   const count = useMystore(state => state.count)
+   const increment = useMystore(state  => state.increment)
+   const capitalizeName = useMystore(state => state.capitalizeName)
 
-    const handleClick = (()=>{
-        state.increment();
-    })
-
-    const handleNameClick = (()=>{
-        state.capitalizeName();
-    })
+   useEffect(()=>{
+    console.log("re render" ,count);
+   },[count])
 
   return (
     <div>
-        <p className='font-semibold font-serif text-xl'>{state.name}</p>
-        <p className='font-semibold font-serif text-xl mt-4'>{state.count}</p>
+        <p className='font-semibold font-serif text-xl'>{count}</p>
+        {/* <p className='font-semibold font-serif text-xl mt-4'>{state.count}</p> */}
         <button
-        onClick={handleClick} 
+        onClick={increment} 
         className='border-3 border-sky-400 rounded-xl px-4 py-2 mt-6 font-semibold active:translate-y-2 active:text-sky-800   active:border-sky-800 transition '>
             Increment
         </button>
         <br />
         <button
-        onClick={handleNameClick} 
+        onClick={capitalizeName} 
         className='border-3 border-sky-400 rounded-xl px-4 py-2 mt-6 font-semibold active:translate-y-2 active:text-sky-800   active:border-sky-800 transition '>
             Capitalize
         </button>
