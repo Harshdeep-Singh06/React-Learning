7088ba9e21212e5e3736c0ce386d1c85d4acb9b0
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-07 14:31:04 +0530

    Create to input to double the count

--- react-useMemo-project/src/App.jsx
+++ react-useMemo-project/src/App.jsx
@@ -4,6 +4,18 @@ import './App.css'
 
 function App() {
   const[count, setCount] = useState(0);
+  const[input, setInput] = useState(0);
+
+    function expensiveTask(num){
+      console.log("Inside expensive taks");
+      for(let i = 0; i<=100000000; i++){
+
+      }
+        return num*2;
+    }
+
+    let doubleValue = expensiveTask(input);
+
   
 
   return (
@@ -13,10 +25,31 @@ function App() {
     className='border-2 px-4 py-2 font-semibold rounded-xl bg-green-400 border-yellow-300 active:translate-y-2 transition'>
       Increment 
     </button>
+
     <div className='border-2 border-green-300 px-4 py-2 rounded-xl mt-4'>
+
       <p className='text-green-400'>
         Count: {count}
       </p>
+
+    </div>
+
+    <div>
+      <input 
+      type='number'
+      placeholder='enter number'
+      value={input}
+      onChange={(e)=>setInput(e.target.value)}
+      className='border-2 border-purple-400 rounded-xl px-4 text-purple-400 mt-4 outline-none'>
+      </input>
+    </div>
+
+    <div className='border-2 border-green-300 px-2 py-2 rounded-xl mt-4 '>
+
+       <p className='text-yellow-400'>
+        Double: {doubleValue}
+      </p>
+
     </div>
    </div>
   )
