6565b9961a4d91790bfc40f6f16d29f7b2670acf
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-07 20:39:00 +0530

    Create increment button

--- react-usecallback-project/src/App.jsx
+++ react-usecallback-project/src/App.jsx
@@ -4,11 +4,22 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  function handleClick(){
+    setCount(count + 1);
+  }
+
   return (
    <div className='flex flex-col justify-center items-center min-h-screen bg-black'>
-    <h1 className='text-purple-400'>
-      hello  jee
-    </h1>
+   <button
+   onClick={handleClick} 
+   className='px-4 py-2 rounded-xl border-2 border-purple-400 text-purple-400 font-semibold active:translate-y-2 transition duration-300'>
+    Increment
+   </button>
+   <div>
+    <p className='px-4 py-2 border border-purple-400 rounded-2xl text-purple-400  mt-4'>
+      Count: {count}
+    </p>
+   </div>
    </div>
   )
 }
